712e6a7b4532ac53c1a80aa06112bb76301f0e45
Juli <julinlenovo@gmail.com>
2024-06-04 08:19:56 +0000

    meow

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 $counter_name = "about_visitors.txt";
-if (!file_exists($counter_name)) {
-  $f = fopen($counter_name, "w");
-  fwrite($f,"0");
-  fclose($f);
-}
 $f = fopen($counter_name,"r");
 $counterVal = fread($f, filesize($counter_name));
 fclose($f);
